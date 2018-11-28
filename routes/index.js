4848ba32ddb98648fc79c57822de06bb33d482d8
Hawk <cph-ol31@cphbusiness.dk>
2018-11-28 15:40:47 +0100

    Trying to fix heroku

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,17 @@ require('../dbSetup')(require("../settings").TEST_DB_URI);;
 
 
 /* USER */
-import { registerEndPoint } from './user_routes';
 
 // Register
-router.post('/api/register', registerEndPoint)
+router.post('/api/register', async function (req, res, next) {
+    var body = req.body;
+    var result = await userFacade.addUser(body.firstname, body.lastname, body.username, body.password, body.email)
+    if (!result) {
+      res.send(JSON.stringify({ status: "User has not been registered, because the username already exists.", error: true }))
+    } else {
+      res.send(JSON.stringify({ status: "User has been succesfully registered", error: false }))
+    }
+})
 
 // Login
 router.post('/api/login', async function (req, res, next) {
